85eca618339c5fd518e406dd0d176d02f0ab717e
Barbara <barbara_carrizo@outlook.com>
2025-06-16 19:38:37 -0300

    Proyecto base Unistock generado desde Spring Initializr

---
+++ unistock/src/test/java/com/app/unistock/UnistockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.unistock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnistockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
